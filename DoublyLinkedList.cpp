a34732f63afc3929afec2d43c8c7d4ed24ddc237
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-05-15 10:38:33 +0700

    Menambahkan Function traverse

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -99,7 +99,36 @@ class DoubleLinkedList
                 current = current->next;
 
             if (current == NULL)
+            {
+                cout << "Record not found" << endl;
+                return;
+            }
+
+            // Step 2: If node is at the beginning
+            if (current == START)
+            {
+                START = current->next; // Step 2a: START = START.next
+                if (START != NULL)
+                    START->prev = NULL; // Step 2b: START.prev = NULL
+            }
+            else
+            {
+                // Step 3: Link previous node to next of current
+                current->prev->next = current->next;
+
+                // Step 4: If current is not the last node
+                if (current->next != NULL)
+                    current->next->prev = current->prev;
+            }
+
+            void traverse()
+            {
+                if (START == NULL)
             
+        }
+        
+};
+
 
         
 
