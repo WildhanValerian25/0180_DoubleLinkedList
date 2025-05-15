48418729c1c2239ac49cca9aec55dfab721aeb33
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-05-15 10:29:10 +0700

    Menambahkan function Hapus

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -27,8 +27,82 @@ class DoubleLinkedList
             cout << "\nEnter the roll number of the student ";
             cin >> nim;
 
-         
-};
+            // Step 1: Allocate memory for new mode
+            Node *newNode = new Node();
+
+            // Step 2: Assign value to the data fields
+            newNode->noMhs = nim;
+
+            // Step 3: Insert at beginning if list is empty or nim is smallest
+            if (START == NULL || nim <= START->noMhs)
+            {
+                if (START != NULL && nim == START->noMhs)
+                {
+                    cout << "\nDuplicate number not allowed" << endl;
+                    return;
+                }
+                // Step 4: newNode.next = START
+                newNode->next = START;
+
+                // Step 5: START.prev = newNode (if START exits)
+                if (START != NULL)
+                    START->prev = newNode;
+                
+                // Step 6: newNode.prev = NULL
+                newNode->prev = NULL;
+
+                // Step 7: START = newNode
+                START = newNode;
+                return;
+            }
+            // insert in between node
+            // Step 8: Locate position for insertion
+            Node *current = START;
+            while (current->next != NULL && current->next->noMhs < nim)
+            {
+                current = current->next;
+            }
+
+            if (current->next != NULL &&nim == current->next->noMhs)
+            {
+                cout << "\nDuplicate roll numbers not allowed" << endl;
+                return;
+            }
+
+
+
+            // Step 9: Insert between current and current->next
+            newNode->next = current->next; // Step 9a: newNode.next = current.next
+            newNode->prev = current;       // Step 9b: newNode.prev = current
+
+            // insert last node 
+            if (current->next != NULL)
+                current->next->prev = newNode; // Step 9d: current.next = newNode
+        }
+
+        void hapus()
+        {
+            if (START == NULL)
+            {
+                cout << "\nList is empty" << endl;
+                return;
+            }
+
+            cout << "\nEnter the roll number of the student whose record is to be deleted ";
+            int rollNo;
+            cin >> rollNo;
+
+            Node *current = START;
+
+            // Step 1: Traverse the list to find the node 
+            while (current != NULL && current->noMhs != rollNo)
+                current = current->next;
+
+            if (current == NULL)
+            
+
+        
+
         
  
         
