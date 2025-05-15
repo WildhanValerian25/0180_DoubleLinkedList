e5c7110568b99b7f93ff136054488cada58979f7
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-05-15 10:50:24 +0700

    Mendeklarasikan Start

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -124,17 +124,16 @@ class DoubleLinkedList
             void traverse()
             {
                 if (START == NULL)
+                {
+                    cout << "\nList is empty" << endl;
+                    return;
+                }
+          
+            
             
         }
         
 };
-
-
-        
-
-        
- 
-        
 
 int main (){
 
