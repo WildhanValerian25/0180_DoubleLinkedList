5b1164ecdf2ff10afd5499656f35e1b3acd4b74a
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-05-15 11:01:45 +0700

    Memindahkan ke last node

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -161,6 +161,16 @@ class DoubleLinkedList
 
                     // Step 2: Traverse backward 
                     cout << "\nRecords in descending order of roll number are:\n";
+                    while (currentNode != NULL)
+                    {
+                        cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+
+                        // Step 3: Move to previous node
+                        currentNode = currenNode->prev;
+                        i--;
+                    }
+
+                    
                 }
 
             }
